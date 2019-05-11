e44e7c24a81e1d92ab1677cb3dad0477bea40149
VonC <vonc@laposte.net>
2019-05-11 04:08:10 +0200

    enumerate *all* child windows

--- go.mod
+++ go.mod
@@ -1,6 +1,7 @@
 module winpos
 
 require (
+	github.com/davecgh/go-spew v1.1.1
 	github.com/lxn/win v0.0.0-20190508144640-5d15a47a4bff
 	golang.org/x/sys v0.0.0-20190509141414-a5b02f93d862 // indirect
 )
--- main.go
+++ main.go
@@ -3,15 +3,20 @@ package main
 import (
 	"fmt"
 	"image"
+	"log"
 	"syscall"
 	"unsafe"
 
+	"github.com/davecgh/go-spew/spew"
 	"github.com/lxn/win"
 )
 
 var (
-	libUser32, _               = syscall.LoadLibrary("user32.dll")
-	funcEnumDisplayMonitors, _ = syscall.GetProcAddress(syscall.Handle(libUser32), "EnumDisplayMonitors")
+	libUser32, _                = syscall.LoadLibrary("user32.dll")
+	funcEnumDisplayMonitors, _  = syscall.GetProcAddress(syscall.Handle(libUser32), "EnumDisplayMonitors")
+	funcGetDesktopWindow, _     = syscall.GetProcAddress(syscall.Handle(libUser32), "GetDesktopWindow")
+	funcGetWindowTextW, _       = syscall.GetProcAddress(syscall.Handle(libUser32), "GetWindowTextW")
+	funcGetWindowTextLengthW, _ = syscall.GetProcAddress(syscall.Handle(libUser32), "GetWindowTextLengthW")
 )
 
 func main() {
@@ -21,6 +26,49 @@ func main() {
 	for i := 0; i < ndisplays; i++ {
 		fmt.Printf("Display #'%d': bounds '%+v'\n", i+1, getDisplayBounds(i))
 	}
+
+	hwnd := getDesktopWindow()
+	hdc := win.GetDC(hwnd)
+	if hdc == 0 {
+		log.Fatalf("GetDC failed")
+	}
+	defer win.ReleaseDC(hwnd, hdc)
+	// https://github.com/lxn/win/issues/19
+	spew.Dump(hwnd)
+
+	win.EnumChildWindows(hwnd, syscall.NewCallback(printme), 0)
+}
+
+func getDesktopWindow() win.HWND {
+	ret, _, _ := syscall.Syscall(funcGetDesktopWindow, 0, 0, 0, 0)
+	return win.HWND(ret)
+}
+
+func printme(hwnd uintptr, lParam uintptr) uintptr {
+	spew.Dump(hwnd)
+	fmt.Printf("getWindowText: '%s'\n", getWindowText(hwnd))
+	return 1 // true to continue
+}
+
+func getWindowText(hwnd uintptr) string {
+	iLen := getWindowTextLength(hwnd) + 1
+	buf := make([]uint16, iLen)
+
+	if _, _, err := syscall.Syscall(uintptr(funcGetWindowTextW), 3, uintptr(hwnd), uintptr(unsafe.Pointer(&buf[0])), uintptr(iLen)); err != 0 {
+		log.Fatalf("Call GetWindowText failed:" + syscall.Errno(err).Error())
+	}
+	return syscall.UTF16ToString(buf)
+}
+
+func getWindowTextLength(hwnd uintptr) int {
+
+	var ret uintptr
+	var err syscall.Errno
+	if ret, _, err = syscall.Syscall(uintptr(funcGetWindowTextLengthW), 1, uintptr(hwnd), 0, 0); err != 0 {
+		log.Fatalf("Call GetWindowTextLengthW failed:" + syscall.Errno(err).Error())
+	}
+
+	return int(ret)
 }
 
 // https://github.com/kbinani/screenshot/blob/9ef8b9209e372fbb0c126cc2648e33bece0c9660/screenshot_windows.go
